fc102f3e0cfe74f32a1c2da105af2ce91232cadd
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-09 18:50:51 +0100

    l107: SQLAlchemy UserModel

--- section6/code/resources/user.py
+++ section6/code/resources/user.py
@@ -20,13 +20,8 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "INSERT INTO users VALUES (NULL,?,?)"
-        cursor.execute(query, (data['username'], data['password'],))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data) #thanks to parser we'll always have 'username' and 'password', that's why we can unpack
+        user.save_to_db()
 
         return {"message": "User created successfully."}, 201
