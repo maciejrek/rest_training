22342b79a03f6cdc2ed1bead3ab033d66e040ef6
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-09 12:47:00 +0100

    section4

---
+++ section4/code/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'temp'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+items = []
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
+
+    @jwt_required()  # jwt - for security
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404  # code means that resource's not found
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f"An item with name '{name}' already exists."}, 400  # bad request code
+
+        data = Item.parser.parse_args()
+        # force=True - content-type header is not needed,
+        # silent=True - doesn't return error, just None
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201  # code means that object has been created
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        # make sure that the price element is the only one we'll take care of
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
